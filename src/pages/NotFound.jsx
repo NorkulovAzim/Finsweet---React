125fd2ac7f87d8b4fe3e9b19452fc4fdebe48e08
azimnorqulov <azim.norqulov@icloud.com>
2025-11-01 14:04:59 +0500

    not found add

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <section className="notfound-section">
+      <div className="notfound-content">
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>
+          Oops! The page you are looking for doesn’t exist or may have been
+          moved.
+        </p>
+        <a href="/" className="back-home-btn">
+          Go Back Home
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
